5e287cf00adb18a5a7a58487b247ab5b54457dd4
harrier999 <harrier999@naver.com>
2024-01-22 17:51:34 +0900

    Add ThemeData

--- lib/login.dart
+++ lib/login.dart
@@ -8,23 +8,31 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "user_id",
+        body: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Get.back(),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "user_id",
+                  ),
+                  controller: myController,
                 ),
-                controller: myController,
-              ),
-              FloatingActionButton(
-                onPressed: () => Get.to(
-                  Rooms(),
-                  arguments: {"user_id": myController.text},
+                FloatingActionButton(
+                  onPressed: () => Get.to(
+                    Rooms(),
+                    arguments: {"user_id": myController.text},
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       );
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'beforeLogin/firstPage.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -10,14 +11,32 @@ void main() async {
   runApp(const MyApp());
 }
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) => GetMaterialApp(
+//         home: Scaffold(
+//           appBar: AppBar(),
+//           body: LoginPage(),
+//         ),
+//       );
+// }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) => GetMaterialApp(
-        home: Scaffold(
-          appBar: AppBar(),
-          body: LoginPage(),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            //1
+            primarySwatch: Colors.orange, //2
+            accentColor: Colors.orangeAccent, //4
+          ).copyWith(
+            secondary: Colors.amber,
+          ),
         ),
+        home: FirstPage(),
       );
 }
